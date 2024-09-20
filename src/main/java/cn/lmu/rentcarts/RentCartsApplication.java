09a4ed2bffeb5676038b730905db524f1c50682d
Misakiop <atianyar@qq.com>
2024-09-20 11:34:24 +0800

    0.1

---
+++ src/main/java/cn/lmu/rentcarts/RentCartsApplication.java
@@ -0,0 +1,13 @@
+package cn.lmu.rentcarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lmu/rentcarts/RentCartsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lmu.rentcarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
